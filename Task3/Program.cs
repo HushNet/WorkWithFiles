a28bc3cc4f6728b9ade9568ec7436e1854eaf81d
dotBuddy <pakhomov.alx@mail.ru>
2022-07-10 16:52:11 +0300

    Final 1

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Channels;
+using Task2;
+
+string path = @"C:\Users\Alexey\Desktop\hohoho";
+SizeCalculator sc = new SizeCalculator();
+
+Console.WriteLine("Размер до удаления: ");
+Console.WriteLine(sc.CalculateSizeOfFolder(path));
+
+
+sc.CleanFilesNotUsed(path);
+
+Console.WriteLine("Размер после удаления: ");
+Console.WriteLine(sc.CalculateSizeOfFolder(path));
---
+++ Task3/SizeCalculator.cs
@@ -0,0 +1,83 @@
+﻿namespace Task2;
+
+public class SizeCalculator
+{
+    public long CalculateSizeOfFolder(string path)
+    {
+        long totalSize = 0;
+        var directoryInfo = new DirectoryInfo(path);
+        var catalogs = directoryInfo.GetDirectories();
+        var files = directoryInfo.GetFiles();
+
+        foreach (var file in files)
+        {
+            if (file.Exists)
+            {
+                totalSize += file.Length;
+            }
+        }
+        
+        foreach (var cat in catalogs)
+        {
+            if (cat.Exists)
+            {
+                totalSize += CalculateSizeOfFolder(cat.FullName);
+            }
+        }
+        
+        return totalSize;
+    }
+    
+    public void CleanFilesNotUsed(string path)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(path);
+        var catalogs = directoryInfo.GetDirectories();
+        var files = directoryInfo.GetFiles();
+
+        foreach (var file in files)
+        {
+            if (file.Exists)
+            {
+                if (Math.Abs(file.LastWriteTime.Subtract(DateTime.Now).TotalMinutes) > 30)
+                {
+                    try
+                    {
+                        file.Delete();
+                        Console.WriteLine($"Файл {file.Name} удален");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                    
+                }
+            }
+            
+        }
+        
+        foreach (var cat in catalogs)
+        {
+            if (cat.Exists)
+            {
+                CleanFilesNotUsed(cat.FullName);
+                if (Math.Abs(cat.LastWriteTime.Subtract(DateTime.Now).TotalMinutes) > 30 && cat.GetFiles().Length == 0)
+                {
+                    try
+                    {
+                        cat.Delete();
+                        Console.WriteLine($"Каталог {cat.Name} удален");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                        
+                }
+            }
+                
+        }
+
+    }
+}
