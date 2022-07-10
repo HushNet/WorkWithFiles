d850f6cd4c98a3cb8f787210d03cc19a85a56656
dotBuddy <pakhomov.alx@mail.ru>
2022-07-10 16:27:17 +0300

    Final 1

---
+++ Task1/FilesCleaner.cs
@@ -0,0 +1,57 @@
+﻿namespace Task1;
+
+public class FilesCleaner
+{
+    public void CleanFilesNotUsed(DirectoryInfo directoryInfo)
+    {
+
+        var catalogs = directoryInfo.GetDirectories();
+        var files = directoryInfo.GetFiles();
+
+        foreach (var file in files)
+        {
+            if (file.Exists)
+            {
+                if (Math.Abs(file.LastWriteTime.Subtract(DateTime.Now).TotalMinutes) > 30)
+                {
+                    try
+                    {
+                        file.Delete();
+                        Console.WriteLine($"Файл {file.Name} удален");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                    
+                }
+            }
+            
+        }
+        
+            foreach (var cat in catalogs)
+            {
+                if (cat.Exists)
+                {
+                    CleanFilesNotUsed(cat);
+                    if (Math.Abs(cat.LastWriteTime.Subtract(DateTime.Now).TotalMinutes) > 30 && cat.GetFiles().Length == 0)
+                    {
+                        try
+                        {
+                            cat.Delete();
+                            Console.WriteLine($"Каталог {cat.Name} удален");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+                        
+                    }
+                }
+                
+            }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Task1;
+
+DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\Alexey\Desktop\hohoho");
+
+FilesCleaner filesCleaner = new FilesCleaner();
+
+filesCleaner.CleanFilesNotUsed(directoryInfo);
