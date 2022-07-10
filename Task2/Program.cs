098951fc4584062a7a227995535f0cd87e273dac
dotBuddy <pakhomov.alx@mail.ru>
2022-07-10 16:41:20 +0300

    Final 1

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Channels;
+using Task2;
+
+string path = @"C:\Users\Alexey\Desktop\hohoho";
+SizeCalculator sc = new SizeCalculator();
+
+Console.WriteLine(sc.CalculateSizeOfFolder(path));
---
+++ Task2/SizeCalculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Task2;
+
+public class SizeCalculator
+{
+    public long totalSize = 0;
+
+    public long CalculateSizeOfFolder(string path)
+    {
+        var directoryInfo = new DirectoryInfo(path);
+        var catalogs = directoryInfo.GetDirectories();
+        var files = directoryInfo.GetFiles();
+
+        foreach (var file in files)
+        {
+            if (file.Exists)
+            {
+                totalSize += file.Length;
+            }
+        }
+        
+        foreach (var cat in catalogs)
+        {
+            if (cat.Exists)
+            {
+                totalSize += CalculateSizeOfFolder(cat.FullName);
+            }
+        }
+
+        return totalSize;
+    }
+}
